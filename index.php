4829f99bda9ca57f4fede084c8c2426a2590054a
John Minnich <spellingace91@gmail.com>
2013-11-02 15:21:21 -0700

    Initial commit
    Includes the home page and accompanying CSS.

---
+++ index.php
@@ -0,0 +1,103 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<title>St. Francis de Sales Parish - Catholic Church, Tucson, AZ</title>
+	
+		<meta charset="utf-8" />
+		<meta name="description" content="ADD CONTENT HERE"/>
+		<meta name="viewport" content="initial-scale=1.0, width=device-width"/>
+		
+		<link rel="icon" href="images/favicon.ico" type="image/x-icon"/>
+		<link rel="shortcut icon" href="images/favicon.ico"/>
+		<link rel="icon" type="image/ico" href="images/favicon.ico"/>
+		
+		<link rel="stylesheet" href="css/reset.css" />
+		<link rel="stylesheet" href="css/base.css" />
+		
+		<link href='http://fonts.googleapis.com/css?family=Lusitana' rel='stylesheet' type='text/css'>
+		<link href='http://fonts.googleapis.com/css?family=Source+Sans+Pro:400,500' rel='stylesheet' type='text/css'>
+	</head>
+	<body>
+		<header>
+			<div class="page" id="parish-info">
+				<div id="parish-name">
+					<h1>
+						<a href="#">St. Francis de Sales Parish</a>
+					</h1>
+					<h2>A Catholic Church in the Diocese of Tucson</h2>
+				</div>
+				
+				<div id="parish-contact">
+					<h3>1375 S. Camino Seco<br/>Tucson, AZ 85710</h3>
+					<h3>(520) 885-5908</h3>
+				</div>
+			</div>
+			
+			<div id="nav">
+				<ul class="page">
+					<li>
+						<a href="#">Home</a>
+					</li>
+					<li>
+						<a href="#">Sacraments</a>
+					</li>
+					<li>
+						<a href="#">Ministries</a>
+					</li>
+					<li>
+						<a href="#">Resources</a>
+					</li>
+					<li>
+						<a href="#">Giving</a>
+					</li>
+					<li>
+						<a href="#">About</a>
+					</li>
+					<li>
+						<a href="#">Contact</a>
+					</li>
+				</ul>
+			</div>
+		</header>
+		<div id="wrapper">
+			<section>
+				<div>
+					<p>First thing</p>
+				</div>
+				<div>
+					<p>Second thing</p>
+				</div>
+				<div>
+					<p>Some stuff here</p>
+				</div>
+			</section>
+		</div>
+		<div id="nav-mobile">
+			<ul class="page">
+				<li>
+					<a href="#">Sacraments</a>
+				</li>
+				<li>
+					<a href="#">Ministries</a>
+				</li>
+				<li>
+					<a href="#">Resources</a>
+				</li>
+				<li>
+					<a href="#">Giving</a>
+				</li>
+				<li>
+					<a href="#">About</a>
+				</li>
+				<li>
+					<a href="#">Contact</a>
+				</li>
+			</ul>
+		</div>
+		<footer>
+			<div class="page">
+				<p>&copy; 2013 St. Francis de Sales Parish</p>
+			</div>
+		</footer>
+	</body>
+</html>
