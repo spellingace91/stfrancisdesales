ed1b72044be3f3bde0118b677767ef143d23190d
John Minnich <spellingace91@gmail.com>
2013-11-22 11:42:22 -0700

    First draft of a two-column schedule
    - two columns, with adequate spacing, but inadequate text positioning
    (more treatment needed on the parenthesized text following times so
    that all times can appear uniform under their respective days).
    
    - The two-column schedule, though pertinent, may disrupt the flow of
    the page and be out of order in the information hierarchy. Further
    experimentation will commence to decide exactly where they should go.

--- inc/functions.php
+++ inc/functions.php
@@ -4,13 +4,13 @@
 		$variable = false;
 		if (!$variable && (10 <= date(n) || date(n) <= 4))
 		{
-			echo '<div class="schedule wrapper-section wrapper-box three">
-	<h2>Mass</h2>
+			echo '<div class="schedule wrapper-section wrapper-box two" id="mass">
+	<h2>Holy Sacrifice of the Mass</h2>
 	<div class="times-content">
 		<div class="day" id="saturday">
 			<p class="day-of-the-week">Saturday</p>
 			<p class="times">8:00 am</p>
-			<p class="times">5:00 pm <span>(Vigil)</span></p>
+			<p class="times">5:00 pm <span>(Anticipated)</span></p>
 		</div>
 	
 		<div class="day" id="sunday">
@@ -21,8 +21,8 @@
 			<p class="times">5:00 pm</p>
 		</div>
 		
-		<div class="day">
-			<p class="day-of-the-week" style="clear:both;">Monday - Friday</p>
+		<div class="day" id="mon-fri">
+			<p class="day-of-the-week">Monday - Friday</p>
 			<p class="times">6:30 am <span>(Communion Service)</span></p>
 			<p class="times">8:00 am</p>
 		</div>
@@ -30,7 +30,7 @@
 	</div>
 </div>
 
-<div class="schedule wrapper-section wrapper-box three">
+<div class="schedule wrapper-section wrapper-box two" id="reconciliation">
 	<h2>Reconciliation</h2>
 	<div class="times-content">
 		<div class="day">
@@ -38,18 +38,6 @@
 			<p class="times">3:00 pm</p>
 			<p class="times">by appointment</p>
 		</div>
-	</div>
-</div>
-
-<div class="schedule wrapper-section wrapper-box three">
-	<h2>Adoration</h2>
-	<div class="times-content">
-		
-		<div class="day">
-			<p class="day-of-the-week" >Tuesdays</p>
-			<p class="times">After 8:00 am Mass - 5:00 pm</p>
-		</div>
-	
 	</div>
 </div>';
 		} else {
