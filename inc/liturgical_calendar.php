377e48ddadd05770b42177855500b2f7b6484ac0
pipcorona <pipcorona@gmail.com>
2014-01-29 12:33:57 -0700

    More calendar stuff
    -Added all holy days of obligation. General date calculations are
    complete. Time to get fancy.

--- inc/liturgical_calendar.php
+++ inc/liturgical_calendar.php
@@ -4,14 +4,18 @@
 	echo 'Holy Thursday: '.date("l, F j, Y" ,strtotime("last thursday", easter_date(date("Y")))).'<br />';
 	echo 'Good Friday: '.date("l, F j, Y" ,strtotime("last friday", easter_date(date("Y")))).'<br />';
 	echo 'Easter: '.date("l, F j, Y" ,easter_date(date("Y"))).'<br />';
-	echo 'Ascension: '.date("l, F j, Y" ,strtotime("+39 days", easter_date(date("Y")))).'<br />';
+	echo 'Ascension*: '.date("l, F j, Y" ,strtotime("+39 days", easter_date(date("Y")))).'<br />';
 	echo 'Pentecost: '.date("l, F j, Y" ,strtotime("+49 days", easter_date(date("Y")))).'<br />';
+	echo 'Solemnity of the Assumption of the Blessed Virgin Mary*: '.date("l, F j, Y" ,mktime(0,0,0,8,15,date("Y"))).'<br />';
+	echo 'Solemnity of all Saints*: '.date("l, F j, Y" ,mktime(0,0,0,11,1,date("Y"))).'<br />';
+	echo 'Solemnity of the Immaculate Conception*: '.date("l, F j, Y" ,mktime(0,0,0,11,8,date("Y"))).'<br />';
 	echo 'Advent (1st): '.date("l, F j, Y" ,strtotime("-4 weeks sunday", mktime(0,0,0,12,25,date("Y")))).'<br />';
 	echo 'Advent (2nd): '.date("l, F j, Y" ,strtotime("-3 weeks sunday", mktime(0,0,0,12,25,date("Y")))).'<br />';
 	echo 'Advent (3rd): '.date("l, F j, Y" ,strtotime("-2 weeks sunday", mktime(0,0,0,12,25,date("Y")))).'<br />';
 	echo 'Advent (4th): '.date("l, F j, Y" ,strtotime("-1 weeks sunday", mktime(0,0,0,12,25,date("Y")))).'<br />';
-	echo 'Christmas: '.date("l, F j, Y" ,mktime(0,0,0,12,25,date("Y"))).'<br />';
+	echo 'Christmas*: '.date("l, F j, Y" ,mktime(0,0,0,12,25,date("Y"))).'<br />';
 	echo 'Epiphany: '.date("l, F j, Y" ,strtotime("+12 days", mktime(0,0,0,12,25,date("Y")))).'<br />';
+	echo 'Solemnity of Mary, Mother of God*: '.date("l, F j, Y" ,mktime(0,0,0,1,1,date("Y"))).'<br />';
 	if(date("l" ,strtotime("+12 days", mktime(0,0,0,12,25,date("Y"))))=="Sunday"){
 		echo 'Feast of the Baptism of Christ: '.date("l, F j, Y" ,strtotime("+13 days", mktime(0,0,0,12,25,date("Y")))).'<br />';
 	}else{
