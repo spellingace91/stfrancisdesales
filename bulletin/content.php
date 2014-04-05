5883b7d4809d10cb858482db41309993b05efe5b
John Minnich <spellingace91@gmail.com>
2014-04-05 16:28:18 -0700

    Bulletin template chages and minified CSS

--- bulletin/content.php
+++ bulletin/content.php
@@ -3,7 +3,7 @@
 								$i = 1;
 								while ($i < 7) {
 									$date = (date(D)=="Sun" ? date(Ymd).B : date(Ymd, strtotime('-'.$i.' week sunday')));
-									echo '<a href="http://content.seekandfind.com/bulletins/05/0387/'.$date.'B.pdf">'.date("F j, Y", strtotime($date)).'</a><br />';
+									echo '<a class="bulletin-link" href="http://content.seekandfind.com/bulletins/05/0387/'.$date.'B.pdf">'.date("F j, Y", strtotime($date)).'</a><br />';
 									$i++;
 								}
 							?>
--- inc/head.php
+++ inc/head.php
@@ -9,16 +9,22 @@
 		<link rel="shortcut icon" href="/stfrancisdesales/images/favicon.ico"/>
 		<link rel="icon" type="image/ico" href="/stfrancisdesales/images/favicon.ico"/>
 		
-		<link rel="stylesheet" href="/stfrancisdesales/css/reset.css" />
+		<!--
+<link rel="stylesheet" href="/stfrancisdesales/css/reset.css" />
 		<link rel="stylesheet" href="/stfrancisdesales/css/base.css" />
 		<link rel="stylesheet" href="/stfrancisdesales/css/skeleton.css" />
 		<link rel="stylesheet" href="/stfrancisdesales/css/layout.css" />
+-->
+		
+		<link type="text/css" rel="stylesheet" href="min/b=stfrancisdesales/css&f=reset.css,skeleton.css,base.css,layout.css" />
 		
 		<?php include $inc.'functions.php'; ?>
 		
-		<link href='http://fonts.googleapis.com/css?family=Lusitana' rel='stylesheet' type='text/css'>
-		<link href='http://fonts.googleapis.com/css?family=Source+Sans+Pro' rel='stylesheet' type='text/css'>
+		<link href='http://fonts.googleapis.com/css?family=Lusitana|Source+Sans+Pro' rel='stylesheet' type='text/css'>
 		
 		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
-		<script src="/stfrancisdesales/js/backstretch.js"></script>
+		<!--
+<script src="/stfrancisdesales/js/backstretch.js"></script>
 		<script src="/stfrancisdesales/js/functions.js"></script>
+-->
+<script type="text/javascript" src="min/b=stfrancisdesales/js&amp;f=backstretch.js,functions.js"></script>
