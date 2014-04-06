edc1e751468fddbf278e6f5ea9d16a62fae88626
John Minnich <spellingace91@gmail.com>
2014-04-06 12:34:36 -0700

    Commented out minified CSS and JS
    We’ll put it in for production since uploading and configuring
    everything in the code base is unnecessary and laborious.

--- inc/head.php
+++ inc/head.php
@@ -9,22 +9,18 @@
 		<link rel="shortcut icon" href="/stfrancisdesales/images/favicon.ico"/>
 		<link rel="icon" type="image/ico" href="/stfrancisdesales/images/favicon.ico"/>
 		
-		<!--
-<link rel="stylesheet" href="/stfrancisdesales/css/reset.css" />
+		<link rel="stylesheet" href="/stfrancisdesales/css/reset.css" />
 		<link rel="stylesheet" href="/stfrancisdesales/css/base.css" />
 		<link rel="stylesheet" href="/stfrancisdesales/css/skeleton.css" />
 		<link rel="stylesheet" href="/stfrancisdesales/css/layout.css" />
--->
 		
-		<link type="text/css" rel="stylesheet" href="/stfrancisdesales/min/b=stfrancisdesales/css&f=reset.css,skeleton.css,base.css,layout.css" />
+		<!-- <link type="text/css" rel="stylesheet" href="/stfrancisdesales/min/b=stfrancisdesales/css&f=reset.css,skeleton.css,base.css,layout.css" /> -->
 		
 		<?php include $inc.'functions.php'; ?>
 		
 		<link href='http://fonts.googleapis.com/css?family=Lusitana|Source+Sans+Pro' rel='stylesheet' type='text/css'>
 		
 		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
-		<!--
-<script src="/stfrancisdesales/js/backstretch.js"></script>
+		<script src="/stfrancisdesales/js/backstretch.js"></script>
 		<script src="/stfrancisdesales/js/functions.js"></script>
--->
-<script type="text/javascript" src="/stfrancisdesales/min/b=stfrancisdesales/js&amp;f=backstretch.js,functions.js"></script>
+<!-- <script type="text/javascript" src="/stfrancisdesales/min/b=stfrancisdesales/js&amp;f=backstretch.js,functions.js"></script> -->
