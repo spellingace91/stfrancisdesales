645385ab853ac80f98fe635b08a00b414bb1419a
pipcorona <pipcorona@gmail.com>
2014-04-08 10:23:28 -0700

    Fix issue #15
    -Updated bulletin logic to work on Sundays

--- bulletin/content.php
+++ bulletin/content.php
@@ -1,9 +1,9 @@
-							<?php
-								date_default_timezone_set('America/Phoenix');
-								$i = 1;
-								while ($i < 7) {
-									$date = (date(D)=="Sun" ? date(Ymd).B : date(Ymd, strtotime('-'.$i.' week sunday')));
-									echo '<a class="bulletin-link" href="http://content.seekandfind.com/bulletins/05/0387/'.$date.'B.pdf">'.date("F j, Y", strtotime($date)).'</a><br />';
-									$i++;
-								}
-							?>
+<?php
+	date_default_timezone_set('America/Phoenix');
+	$i = 1;
+	while ($i < 7) {
+		$date = (date(D)=="Sun" ? date(Ymd, strtotime('-'.($i-1).' week')) : date(Ymd, strtotime('-'.$i.' week sunday')));
+		echo '<a class="bulletin-link" href="http://content.seekandfind.com/bulletins/05/0387/'.$date.'B.pdf">'.date("F j, Y", strtotime($date)).'</a><br />';
+		$i++;
+	}
+?>
