06b6460eba8322e442a98e6e789bc4f3864c2aee
pipcorona <pipcorona@gmail.com>
2014-04-30 08:19:22 -0700

    Bulletin Image Addition
    -Added bulletin image
    —John, you can style it how you want :)

--- bulletin/content.php
+++ bulletin/content.php
@@ -3,7 +3,14 @@
 	$i = 1;
 	while ($i < 7) {
 		$date = (date(D)=="Sun" ? date(Ymd, strtotime('-'.($i-1).' week')) : date(Ymd, strtotime('-'.$i.' week sunday')));
-		echo '<a class="bulletin-link" href="http://content.seekandfind.com/bulletins/05/0387/'.$date.'B.pdf">'.date("F j, Y", strtotime($date)).'</a><br />';
+		echo '
+			<a class="" href="http://content.seekandfind.com/bulletins/05/0387/'.$date.'B.pdf">
+				<img class="" src="http://content.seekandfind.com/bulletins/05/0387/tn_'.$date.'B.jpg" />
+			</a>
+			<a class="bulletin-link" href="http://content.seekandfind.com/bulletins/05/0387/'.$date.'B.pdf">
+				'.date("F j, Y", strtotime($date)).'
+			</a>
+			<br />';
 		$i++;
 	}
 ?>
